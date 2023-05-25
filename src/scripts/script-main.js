6686be8f091a7245f0640b8b7ef9012085bb9d02
Eder Henrique <ederhscc@gmail.com>
2023-05-25 10:27:08 -0300

    Update script-main.js

--- src/scripts/script-main.js
+++ src/scripts/script-main.js
@@ -6,7 +6,7 @@ import { getNumberFollowing } from 'src/scripts/services/get-number-following.js
 import { createEvent } from 'src/scripts/events/create-event.js';
 import { pushEvent } from 'src/scripts/events/push-event.js';
 
-import { user } from '/src/scripts/objects/user.js';
+import { user } from 'src/scripts/objects/user.js';
 import { screen } from '/src/scripts/objects/screen.js';
 
 document.getElementById('btn-search').addEventListener('click', () => {
